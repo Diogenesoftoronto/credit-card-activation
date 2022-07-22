cfb80ce0d963a61fa4a838547b248db5b475bce9
Diogenesoftoronto <87236699+Diogenesoftoronto@users.noreply.github.com>
2022-07-22 12:30:51 -0400

    initial commit

---
+++ server.js
@@ -0,0 +1,11 @@
+const app = require('express')()
+require('dotenv').config()
+const port = process.env.PORT || 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
