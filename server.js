534e22f08c762af0bd05c80d99686ec9f616a096
Diogenesoftoronto <87236699+Diogenesoftoronto@users.noreply.github.com>
2022-07-23 00:36:15 -0400

    attempt to add bulma and fail work with serve static and fail

--- server.js
+++ server.js
@@ -5,19 +5,23 @@ require('dotenv').config()
 const port = process.env.PORT || 3000
 const axios = require('axios')
 const path = require('path')
+const serveStatic = require('serve-static')
+//  config for rendering with ejs
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'html');
+
+app.set('view engine', 'ejs');
+
 async function isCardActivated(req, res) {
   const status = await axios.post(req.body)
   return status
 }
-app.use('/static', express.static(path.join(__dirname, 'public')))
+app.use('/bulma', express.static(__dirname + '/node_modules/bulma/css/'));
 app.get('/', (req, res) => {
-  res.render('./views/index')
+  res.render('index.ejs')
 })
 
 app.post('/activation', (req, res) => {
-  
+  result = isCardActivated(req, res) ? 'success.ejs' : 'failure.ejs' 
   res.render(result)
 })
 
