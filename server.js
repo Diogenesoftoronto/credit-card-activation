8e8a109ea41ef5dcea336ad09bf8e09e3131e495
Diogenesoftoronto <87236699+Diogenesoftoronto@users.noreply.github.com>
2022-07-23 02:18:22 -0400

    add body parser, add default auth key, fix axios request, render properly,
    app is working with all features successfully!

--- server.js
+++ server.js
@@ -6,14 +6,19 @@ const port = process.env.PORT || 3000
 const axios = require('axios')
 const path = require('path')
 const serveStatic = require('serve-static')
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: true }));
 //  config for rendering with ejs
 app.set('views', path.join(__dirname, 'views'));
-
 app.set('view engine', 'ejs');
+// config for axios headers
+axios.defaults.headers.post['Authkey'] = process.env.AUTHKEY
 
-async function isCardActivated(req) {
-  const status = await axios.post('https://us-west2-connexinterview.cloudfunctions.net/cardactivation', req.body)
-  console.log(status)
+async function isCardActivated(data) {
+  const status = await axios.post('https://us-west2-connexinterview.cloudfunctions.net/cardactivation', data)
+  .catch((err)=>console.log("error:", "bad guy"))
+  .then((status)=>
+    console.log("status: nooo"))
   return status
 }
 
@@ -22,8 +27,19 @@ app.get('/', (req, res) => {
 })
 
 app.post('/activation', (req, res) => {
-  result = isCardActivated(req) ? 'success.ejs' : 'failure.ejs' 
-  res.render(result)
+  const data = JSON.stringify(req.body)
+  console.log(data)
+  axios.post('https://us-west2-connexinterview.cloudfunctions.net/cardactivation', data)
+  .then((res) =>{
+   console.log("status: hello", res) 
+   res.render('success.ejs', {cardnumber: req.body.cardnumber, status: res.response.status, message: res.response.data.msg, responsecode: res.response.data.responsecode})
+  }
+    )
+    .catch((err)=>{console.log("error:", err.response.data, err.response.status,)
+  
+    res.render('failure.ejs', {cardnumber: req.body.cardnumber, status: err.response.status, message: err.response.data.msg, responsecode: err.response.data.responsecode})  
+  })
+  
 })
 
 app.listen(port, () => {
