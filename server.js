ed1c489e8d6daed988fa660e2247633d4482b028
Diogenesoftoronto <87236699+Diogenesoftoronto@users.noreply.github.com>
2022-07-23 18:28:09 -0400

    Update server.js
    add comments and remove old comments

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-
+// imports
 const express = require('express')
 const app = express()
 require('dotenv').config()
@@ -6,7 +6,7 @@ const port = process.env.PORT || 3000
 const axios = require('axios')
 const path = require('path')
 const serveStatic = require('serve-static')
-// const bodyParser = require('body-parser');
+
 //  config for rendering with ejs
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
@@ -15,25 +15,24 @@ axios.defaults.headers.post['Authkey'] = process.env.AUTHKEY
 
 app.use(express.urlencoded({ extended: true }));
 
-// convert to async await syntax and pass values to render in post request
+// checks if card is activated and sends the proper ejs template to the server for rendering.
 async function isCardActivated(req, data) {
   try {
     res = await axios.post('https://us-west2-connexinterview.cloudfunctions.net/cardactivation', data)
-    // console.log("response:", res.response.data, res.response.status,)
     const values = {cardnumber: req.body.cardnumber, status: res.response.status, message: res.response.data.msg, responsecode: res.response.data.responsecode}
     return ['success.ejs', values]
   }
   catch (err) {
-    // console.log("error:", err.response.data, err.response.status,)
     const values = {cardnumber: req.body.cardnumber, status: err.response.status, message: err.response.data.msg, responsecode: err.response.data.responsecode}
     return ['failure.ejs', values]
   }
 }
-
+// home route for form page
 app.get('/', (req, res) => {
   res.render('index.ejs')
 })
 
+// post route for activating the credit from the form data on the index
 app.post('/activation', (req, res) => {
   const data = JSON.stringify(req.body)
   const returned = isCardActivated(req, data)
