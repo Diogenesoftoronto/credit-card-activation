5929b383e30b63cc8f35bd1b44263c5d40b5c197
Diogenesoftoronto <87236699+Diogenesoftoronto@users.noreply.github.com>
2022-07-22 23:23:00 -0400

    import azxios and add function and render index for our routes

--- server.js
+++ server.js
@@ -1,9 +1,24 @@
-const app = require('express')()
+
+const express = require('express')
+const app = express()
 require('dotenv').config()
 const port = process.env.PORT || 3000
-
+const axios = require('axios')
+const path = require('path')
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'html');
+async function isCardActivated(req, res) {
+  const status = await axios.post(req.body)
+  return status
+}
+app.use('/static', express.static(path.join(__dirname, 'public')))
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.render('./views/index')
+})
+
+app.post('/activation', (req, res) => {
+  
+  res.render(result)
 })
 
 app.listen(port, () => {
