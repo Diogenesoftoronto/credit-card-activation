d0c89e81ff37688cd88d344f195ba900ab000855
Diogenesoftoronto <87236699+Diogenesoftoronto@users.noreply.github.com>
2022-07-25 15:39:10 -0400

    Update notification.js

--- scripts/notification.js
+++ scripts/notification.js
@@ -2,28 +2,29 @@ const spawnNotification = (title, options) => {
   if (!("Notification" in window)) {
     console.log("This browser does not support desktop notification");
   }
-  else if (Notification.permission === "granted") {
-   new Notification(title, options);
+  else if (Notification.permission == "granted") {
+   const notification = new Notification(title, options);
+  //  select the notification using querySelector
+  //  let notification = document.querySelector('.notification')
+   notification.addEventListener('notificationclick', function(event) {
+     console.log('On notification click: ', event.notification.tag);
+     event.notification.close();
+   
+     // This looks to see if the current is already open and
+     // focuses if it is
+     event.waitUntil(clients.matchAll({
+       type: "window"
+     }).then(function(clientList) {
+       for (const client of clientList) {
+         if (client.url == '/' && 'focus' in client)
+           return client.focus();
+       }
+       if (clients.openWindow)
+         return clients.openWindow('/');
+     }));
+   });
   }
 }
-const notification = querySelector('notification')
-notification.addEventListener('notificationclick', function(event) {
-  console.log('On notification click: ', event.notification.tag);
-  event.notification.close();
-
-  // This looks to see if the current is already open and
-  // focuses if it is
-  event.waitUntil(clients.matchAll({
-    type: "window"
-  }).then(function(clientList) {
-    for (const client of clientList) {
-      if (client.url == '/' && 'focus' in client)
-        return client.focus();
-    }
-    if (clients.openWindow)
-      return clients.openWindow('/');
-  }));
-});
 
 // play sound
 const playSound = (sound) => {
@@ -40,17 +41,24 @@ const playSound = (sound) => {
 //   playSound('/sounds/notification.mp3')
 // }
 // )
-
+const BASE_URL = window.location.href
 
 // select elements
-const submit = querySelector('.submit')
-const form = querySelector('.activation-form')
+const form = document.querySelector('#form')
+// console.log(form)
 
 // add event listener on submit
-submit.addEventListener('submit', function (e) {
+form.addEventListener("submit", (e) => {
+  console.log(Object.keys(e), e)
+  // why is this not working? yyyyyyyyyy this should work
+  Notification.permission == 'denied' ? console.log('please allow notifications')
+  : e.preventDefault()
+
   if (Notification.permission !== 'denied') {
-    e.preventDefault();
-    const data = e.formData;
+    // request permission for notifications
+    Notification.requestPermission().then(function (permission) {
+      // If the user accepts, let's create a notification
+    const data = new FormData(form);
     const cardnumber = data.get("cardnumber")
     const csv = data.get("csv")
     const expirydata = data.get("expirydata")
@@ -64,16 +72,16 @@ submit.addEventListener('submit', function (e) {
     //   // normally authkey would be hidden with env however to do this would require the request to be made off the browser or to another api which I do have! i can make the request to activation route which can send me a response!
     //   // 'Authkey': '<3',
     // };
-    fetch('/notifications', { method: 'post', body: formData })
+    fetch(BASE_URL+'notifications', { method: 'post', body: formData })
     .then((res)=> {
       // spawn notification from request if granted permission
-      if (Notification.permission === 'granted') {
+      if (Notification.permission == 'granted') {
       // play two different sounds depending on the response from the server
         if (res.responsecode === 100) {
-          playSound('/sounds/tailwind.mp3')
+          playSound('sounds/tailwind.mp3')
         }
         else if (res.responsecode === 101) {
-          playSound('/sounds/Hidden Depth.mp3')
+          playSound('sounds/Hidden Depth.mp3')
         }
       spawnNotification('Credit Card Activated', {
         body: 
@@ -83,18 +91,20 @@ submit.addEventListener('submit', function (e) {
         response status is: ${res.status}.`,
         icon: 'images/giphy.gif'
         })
+        console.log(res)
       }
     })
     .catch((err)=>{
       // spawn notification
       if (Notification.permission === 'granted') {
         // play sound when a new notification is spawned
-        playSound('/sounds/What You Deserve.mp3')
+        playSound('sounds/What You Deserve.mp3')
         spawnNotification('Credit Card Failure', {
           body: `Your credit card: ${cardnumber} has not been activated due to error ${err}`,
           icon: 'images/giphy.gif'
           })
     }})
-    .finally(()=>console.log("i forgot the notification functionality and i am adding this last minute. here's some emojis if you are reading this 🕺👯🕺"));
+    .finally(()=>console.log("i forgot the notification functionality and i am adding this last minute. here's some emojis if you are reading this 🕺👯🕺"))
+    })
   }
 })
